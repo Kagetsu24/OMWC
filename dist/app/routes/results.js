fe121913a9411d351999a135f3f38363826f802f
milaneyia <39941577+milaneyia@users.noreply.github.com>
2020-05-29 02:26:51 -0400

    dist

--- dist/app/routes/results.js
+++ dist/app/routes/results.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const router_1 = __importDefault(require("@koa/router"));
-const typeorm_1 = require("typeorm");
 const path_1 = __importDefault(require("path"));
 const Criteria_1 = require("../models/judging/Criteria");
 const Round_1 = require("../models/rounds/Round");
@@ -21,24 +20,20 @@ const downloadSubmission_1 = require("../middlewares/downloadSubmission");
 const resultsRouter = new router_1.default();
 resultsRouter.prefix('/api/results');
 resultsRouter.get('/qualifiers', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c;
-    const qualifier = yield Round_1.Round.findOne({
-        where: {
-            isQualifier: true,
-            resultsAt: typeorm_1.LessThanOrEqual(new Date()),
-        },
-        relations: [
-            'matches',
-            'matches.submissions',
-            'matches.submissions.country',
-            'matches.submissions.qualifierJudging',
-            'matches.submissions.qualifierJudging.judge',
-            'matches.submissions.qualifierJudging.qualifierJudgingToCriterias',
-            'matches.submissions.qualifierJudging.qualifierJudgingToCriterias.criteria',
-        ],
-    });
+    var _a, _b, _c, _d, _e;
+    const qualifier = yield Round_1.Round
+        .createQueryBuilder('round')
+        .innerJoinAndSelect('round.matches', 'matches')
+        .leftJoinAndSelect('matches.submissions', 'submissions', 'round.resultsAt <= :today', { today: new Date() })
+        .leftJoinAndSelect('submissions.country', 'country')
+        .leftJoinAndSelect('submissions.qualifierJudging', 'qualifierJudging')
+        .leftJoinAndSelect('qualifierJudging.judge', 'judge')
+        .leftJoinAndSelect('qualifierJudging.qualifierJudgingToCriterias', 'qualifierJudgingToCriterias')
+        .leftJoinAndSelect('qualifierJudgingToCriterias.criteria', 'criteria')
+        .where('round.isQualifier = true')
+        .getOne();
     const criterias = yield Criteria_1.Criteria.find({});
-    const judges = (_c = (_b = (_a = qualifier === null || qualifier === void 0 ? void 0 : qualifier.matches) === null || _a === void 0 ? void 0 : _a[0].submissions) === null || _b === void 0 ? void 0 : _b[0].qualifierJudging) === null || _c === void 0 ? void 0 : _c.map(j => j.judge);
+    const judges = (_e = (_d = (_c = (_b = (_a = qualifier === null || qualifier === void 0 ? void 0 : qualifier.matches) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b.submissions) === null || _c === void 0 ? void 0 : _c[0]) === null || _d === void 0 ? void 0 : _d.qualifierJudging) === null || _e === void 0 ? void 0 : _e.map(j => j.judge);
     return ctx.body = {
         criterias,
         qualifier,
